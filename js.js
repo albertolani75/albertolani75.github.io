5ccddf6dac693019a5eb59ea744e649f19f62cad
albertolani75 <113071428+albertolani75@users.noreply.github.com>
2022-10-03 11:01:36 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+let edad;
+edad = prompt ("Introduce tu edad ");
+if (edad >= 18){
+    alert ("Puedes entrar, eres mayor de edad.");
+}else{ 
+    alert("Eres menor de edad!! Entrarás al sitio bajo tu responsabilidad!!");
+}
+
+
+   
+
+
+
